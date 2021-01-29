bf1508522cad204db20018420c9cc107ab578fec
Brenton Niebauer <b.niebauer@me.com>
2021-01-29 08:06:34 -0500

    Initial Commit

---
+++ CookieKeeper/CookieKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookieKeeperApp.swift
+//  CookieKeeper
+//
+//  Created by Brenton Niebauer on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct CookieKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
